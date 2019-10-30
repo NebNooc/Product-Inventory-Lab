2a81d362621008f67ea3d69168411b480ea178ec
Ben Coon <bencoon@zipcoders-MacBook-Pro-22.local>
2019-10-30 10:40:58 -0400

    Populated Sneaker Class

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Sneaker {
+
+    private Integer id;
+    private String name;
+    private String brand;
+    private String sport;
+    private Float size;
+    private Integer qty;
+    private Float price;
 }
